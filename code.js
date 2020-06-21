a7d70c227647d9a36819f81e8330355b701eb7c4
Sylwia Kilińska <sylwiak2777@gmail.com>
2020-06-21 09:49:27 +0200

    Initial commit

---
+++ code.js
@@ -0,0 +1,24 @@
+let buttonBackground = document.querySelector(".button");
+let sectionAbout = document.querySelector(".section-about");
+
+buttonBackground.addEventListener("click", () => {
+  sectionAbout.classList.toggle("redBackground");
+  if (buttonBackground.innerText === "Nudny kolor sekcji") {
+    buttonBackground.innerText = "Krwisty kolor sekcji";
+  } else {
+    buttonBackground.innerText = "Nudny kolor sekcji";
+  }
+});
+
+let buttonQuote = document.querySelector(".button__button-quote");
+let quote = document.getElementById("addQuote");
+
+buttonQuote.addEventListener("click", () => {
+  if (quote.style.display === "block") {
+    quote.style.display = "none";
+    buttonQuote.innerText = "Opowiedz mi fraszkę";
+  } else {
+    quote.style.display = "block";
+    buttonQuote.innerText = "Schowaj fraszkę";
+  }
+});
